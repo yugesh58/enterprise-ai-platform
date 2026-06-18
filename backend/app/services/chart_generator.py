99bd20559ca1230d00d9d3f2a80d038d6dd7c8cb
Yugesh <yugesh@Yugeshs-MacBook-Air.local>
2026-06-18 17:35:23 +0530

    Completed Data Analyst Agent with charts

---
+++ backend/app/services/chart_generator.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def generate_chart(result):
+
+    if not isinstance(result, (pd.Series, pd.DataFrame)):
+        return None
+
+    os.makedirs("app/charts", exist_ok=True)
+
+    plt.figure(figsize=(10, 5))
+
+    if isinstance(result, pd.Series):
+        result.plot(kind="bar")
+    else:
+        result.plot()
+
+    plt.tight_layout()
+
+    chart_path = "app/charts/chart.png"
+
+    plt.savefig(chart_path)
+
+    plt.close()
+
+    return chart_path
